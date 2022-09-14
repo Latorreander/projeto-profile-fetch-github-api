a3cdfa49423d5ce68c1bd4be4ac409f4da18ca83
Latorreander <latorreander@gmail.com>
2022-09-14 12:16:54 -0300

    adicionado a busca com repositórios

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -4,26 +4,31 @@ document.getElementById("btn-search").addEventListener("click", () => {
 });
 
 document.getElementById("input-search").addEventListener("keyup", (e) => {
-  const userName = e.target.value
-  const key = e.which || e.keyCode
-  const isEnterKeyPressed = key === 13
+  const userName = e.target.value;
+  const key = e.which || e.keyCode;
+  const isEnterKeyPressed = key === 13;
 
-  if(isEnterKeyPressed){
-    getUserPrifile(userName)
+  if (isEnterKeyPressed) {
+    getUserPrifile(userName);
   }
 });
-
 
 async function user(userName) {
   const response = await fetch(`https://api.github.com/users/${userName}`);
   return await response.json();
 }
 
+async function repos(userName) {
+  const response = await fetch(
+    `https://api.github.com/users/${userName}/repos`
+  );
+  return await response.json();
+}
+
 function getUserPrifile(userName) {
   user(userName).then((userData) => {
-    let userInfo = `<img src="${
-      userData.avatar_url
-    }" alt= "Foto do perfil do usuário"/>
+    let userInfo = `<div class="info">
+                          <img src="${userData.avatar_url}" alt= "Foto do perfil do usuário"/>
                         <div class="data">
                             <h1>${
                               userData.name ?? "Não possui nome cadastrado 😥"
@@ -31,8 +36,28 @@ function getUserPrifile(userName) {
                             <P>${
                               userData.bio ?? "Não possui nome cadastrado 😥"
                             }</p>
+                        </div>
                         </div>`;
 
     document.querySelector(".profile-data").innerHTML = userInfo;
+
+    getUserRepositories(userName);
+  });
+}
+
+function getUserRepositories(userName) {
+  repos(userName).then((reposData) => {
+    let repositoriesItens = "";
+
+    reposData.forEach((repo) => {
+      repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`;
+    });
+
+    document.querySelector(
+      ".profile-data"
+    ).innerHTML += `<div class="repositories section"> 
+                       <h2>Repositórios</h2>
+                        <ul>${repositoriesItens}</ul>
+                    </div>`;
   });
 }
